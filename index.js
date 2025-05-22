d82095b9cb5cc32d987e602961b9ddfd810a842d
Оля <olyaberezina93055@gmail.com>
2025-05-22 18:15:28 +0500

    Добавлен обмен кода VK на access_token через axios

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Сервер работает!');
+  res.send('Добро пожаловать на мой сервер!');
+});
+
+app.get('/ping', (req, res) => {
+  res.send('pong');
+});
+
+// Новый обработчик для VK ID
+app.get('/auth/vk/callback', async (req, res) => {
+  const { code } = req.query;
+  if (!code) {
+    return res.send('Код авторизации не получен!');
+  }
+
+  // !!! Вставь свои данные приложения VK !!!
+  const CLIENT_ID = '53336238';
+  const CLIENT_SECRET = '7sPy0o7CDAs2qYfBCDJC';
+  const REDIRECT_URI = 'https://www.fokusnikaltair.xyz';
+
+  try {
+    const response = await axios.get('https://oauth.vk.com/access_token', {
+      params: {
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+        redirect_uri: REDIRECT_URI,
+        code
+      }
+    });
+
+    const data = response.data;
+    res.send('VK вернул: <pre>' + JSON.stringify(data, null, 2) + '</pre>');
+  } catch (error) {
+    res.send('Ошибка при обмене кода: ' + error.message);
+  }
 });
 
 app.listen(PORT, () => {
-  console.log(`Сервер запущен на порту ${PORT}`);
+  console.log(`Сервер запущен на http://localhost:${PORT}`);
 });
